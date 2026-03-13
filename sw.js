266cfc65d944ecae30f5ab0ab08838d62266b9c1
Shepherdai007 <Shepherdaitimeless@gmail.com>
2026-03-13 11:18:36 -0400

    Upload music hub files

---
+++ sw.js
@@ -0,0 +1,35 @@
+const cacheName="king-emmanuel-app";
+
+self.addEventListener("install",event=>{
+
+event.waitUntil(
+
+caches.open(cacheName).then(cache=>{
+
+return cache.addAll([
+"/",
+"/index.html",
+"/icon-192.png",
+"/icon-512.png"
+]);
+
+})
+
+);
+
+});
+
+
+self.addEventListener("fetch",event=>{
+
+event.respondWith(
+
+caches.match(event.request).then(response=>{
+
+return response || fetch(event.request);
+
+})
+
+);
+
+});
